4f880c7e9cc6accbfc0ff15fee99c0cc40171e95
Manu Datta <manu.datta@gmail.com>
2012-08-06 20:09:46 +0800

    Added row echelon form

--- erl/src/numerl.erl
+++ erl/src/numerl.erl
@@ -47,6 +47,113 @@ remove_nth_col(N,Mat)->
   ,Val.
 minor(I,J,Mat)->
   remove_nth_col(J,remove_nth_row(I,Mat)).
+%iszero
+iszero([H|_]=Mat)->
+ Row=length(Mat)
+ ,Col=length(H)
+ ,ZeroMat = zeros(Row,Col)
+ ,Mat==ZeroMat.
+%
+% row echelon form
+%
+
+% return true if all elements in col are zero at Pth
+is_col_all_zero(_P,[[0]],_) ->
+  true;
+is_col_all_zero(_P,_,0) ->
+  true;
+is_col_all_zero(P,_Mat=[Row|Rows],N) ->
+  {_,[H|_]} = lists:split(P-1,Row)
+  ,case H==0 of
+    true -> is_col_all_zero(P,Rows,N-1);
+    false -> false
+   end.
+is_col_all_zero(P,Mat) ->
+  is_col_all_zero(P,Mat,length(Mat)). 
+% return 1 indexed row which has leading non zero column 
+first_non_zero_col(_,End,End)->
+  -1;
+first_non_zero_col(Mat,P,End)->
+  case is_col_all_zero(P,Mat) of
+    false -> P ;
+    true -> first_non_zero_col(Mat,P+1,End)
+  end.
+first_non_zero_col([H|_]=Mat)->
+  first_non_zero_col(Mat,1,length(H)+1).
+
+first_row_with_non_zero_entry_after_i(_I,_Mat=[],_)->
+  -1;
+first_row_with_non_zero_entry_after_i(I,_Mat=[Row|Rows],Count)->
+  {_,[H|_]} = lists:split(I-1,Row)
+  , case H+0.0 =:= 0.0 of
+    false -> Count;
+    true -> first_row_with_non_zero_entry_after_i(I,Rows,Count+1)
+  end.
+first_row_with_non_zero_entry_after_i(I,_) when 0 >= I ->
+  -1;
+first_row_with_non_zero_entry_after_i(I,Mat)->
+    first_row_with_non_zero_entry_after_i(I,Mat,1).
+%exchange Ith row with first row for Matrix
+exchange_first_row(1,Matrix)-> 
+  Matrix;
+exchange_first_row(I,Matrix)-> 
+  {[First|Rest1],[Ith|Rest2]} = lists:split(I-1,Matrix)
+  ,[Ith|Rest1]++[First|Rest2].
+%make the leading entry 1 by op1 kind operation on matrix rows 
+make_leading_one(I,J,Mat) ->
+  Row = lists:nth(I,Mat)
+  ,Val = lists:nth(J,Row)
+  ,NewRow = [ X/Val || X <- Row]
+  ,{H,[R1|Rest]}=lists:split(I-1,Mat)
+  , case H of
+    [] -> [NewRow|Rest];
+    _ -> lists:append(H,[NewRow|Rest])
+  end.
+%make the leading entry 0 by op2 and op3 kind operation on matrix rows except the given Row Ref 
+make_rest_zeros(P,Row,_Mat=[],Acc)->
+  Acc;
+make_rest_zeros(P,Row,Mat=[Row|Rows],Acc)->
+  make_rest_zeros(P,Row,Rows,[Row|Acc]);
+make_rest_zeros(P,Row,Mat=[First|Rows],Acc)->
+  F = fun()-> 
+        {_,[N|_]} = lists:split(P-1,First)
+        , X = [E*N || E <- Row ]
+        , [ A-B  || {A,B} <- lists:zip(First,X)]
+  end
+ ,make_rest_zeros(P,Row,Rows,[F()|Acc]).
+% main row echelon function     
+ref(Matrix,_Parition=[[]],_CurrentRow)->
+  Matrix;
+ref(Matrix,Partition,CurrentRow)->
+  case length(Matrix) < CurrentRow of
+    true -> Matrix;
+    false ->
+        NonZeroColNum=first_non_zero_col(Partition)
+        ,RowNonZero = first_row_with_non_zero_entry_after_i(NonZeroColNum,Partition)
+        ,Partition1=exchange_first_row(RowNonZero,Partition)
+        ,Partition2=[First|_]=make_leading_one(1,NonZeroColNum,Partition1)
+        ,{L,_}=lists:split(CurrentRow-1,Matrix)
+        ,Partition3= L++Partition2
+        ,NewMatrix=lists:reverse(make_rest_zeros(NonZeroColNum,lists:nth(CurrentRow,Partition3),Partition3,[]))
+        ,{_,Partition4} = lists:split(CurrentRow,NewMatrix)
+         %,io:format("Mat->~p~nPart0->~p~nPart1->~p~nPart2->~p~nPart3->~p~nNewMatrix~p~nNewPart->~p~n",[Matrix,Partition,Partition1,Partition2,Partition3,NewMatrix,Partition4])
+        ,case Partition4 of
+          [] -> NewMatrix;
+          _ -> case iszero(Partition4)  of
+                true -> NewMatrix;
+                false ->ref(NewMatrix,Partition4,CurrentRow+1)
+              end
+        end            
+  end.
+    
+ref(Matrix)->
+  case iszero(Matrix) of
+    true -> Matrix;
+    false -> ref(Matrix,Matrix,1)
+  end.
+%
+% determinant
+%
 det([[Elem]])->
   Elem;
 det(Mat=[Row|_Rows])->
@@ -147,4 +254,8 @@ transpose([H|_]=L)->
   Len = length(H)
   ,[[lists:nth(N,X) || X <- L ] || N <- lists:seq(1,Len)].
 zeros(Row,Col)->
-  [ lists:foldl( fun(_,A) -> [0|A] end , [] , lists:seq(1,Col)) || _X <- lists:seq(1,Row)].
+  [ lists:foldl( fun(_,A) -> [0.0|A] end , [] , lists:seq(1,Col)) || _X <- lists:seq(1,Row)].
+addrow(A,B)->
+  [X+Y || {X,Y} <- lists:zip(A,B)].
+add(A,B)->
+  [addrow(RowA,RowB) || {RowA,RowB} <- lists:zip(A,B)]. 
